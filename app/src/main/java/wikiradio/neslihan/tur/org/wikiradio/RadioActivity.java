810b6088219aa1ca5e200aa66f6e1f34e4f2f719
Neslihan <tur.neslihan@gmail.com>
2016-12-15 16:01:53 +0300

    Initial commit

---
+++ app/src/main/java/wikiradio/neslihan/tur/org/wikiradio/RadioActivity.java
@@ -0,0 +1,13 @@
+package wikiradio.neslihan.tur.org.wikiradio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio);
+    }
+}
