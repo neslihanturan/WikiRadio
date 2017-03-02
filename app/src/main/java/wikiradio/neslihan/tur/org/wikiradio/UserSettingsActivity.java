792f65e51523b049de403841de67eede6d6b337f
Neslihan <tur.neslihan@gmail.com>
2017-03-02 14:44:09 +0300

    Add shared preferences

--- app/src/main/java/wikiradio/neslihan/tur/org/wikiradio/UserSettingsActivity.java
+++ app/src/main/java/wikiradio/neslihan/tur/org/wikiradio/UserSettingsActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 /**
@@ -11,6 +13,7 @@ import android.preference.PreferenceFragment;
  */
 
 public class UserSettingsActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +25,14 @@ public class UserSettingsActivity extends Activity {
         mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
         mFragmentTransaction.commit();
 
+
+
     }
 
     public static class PrefsFragment extends PreferenceFragment {
+        CheckBoxPreference onlyCommons;
+        CheckBoxPreference onlyTTS;
+        CheckBoxPreference both;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,44 @@ public class UserSettingsActivity extends Activity {
 
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preferences);
+
+
+            onlyCommons = (CheckBoxPreference) findPreference("only_commons");
+            onlyTTS = (CheckBoxPreference) findPreference("only_tts");
+            both = (CheckBoxPreference) findPreference("both");
+
+            onlyCommons.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    onlyCommons.setChecked(true);
+                    onlyTTS.setChecked(false);
+                    both.setChecked(false);
+
+                    return true;
+                }
+            });
+
+            onlyTTS.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    onlyCommons.setChecked(false);
+                    onlyTTS.setChecked(true);
+                    both.setChecked(false);
+
+                    return true;
+                }
+            });
+
+            both.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    onlyCommons.setChecked(false);
+                    onlyTTS.setChecked(false);
+                    both.setChecked(true);
+
+                    return true;
+                }
+            });
         }
     }
 }
--- app/src/main/java/wikiradio/neslihan/tur/org/wikiradio/action/AudioSourceSelector.java
+++ app/src/main/java/wikiradio/neslihan/tur/org/wikiradio/action/AudioSourceSelector.java
@@ -1,16 +1,37 @@
 package wikiradio.neslihan.tur.org.wikiradio.action;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 /**
  * Created by nesli on 28.02.2017.
  */
 
 public class AudioSourceSelector {
-    private String selectAudioSource(){
+    private static String selectAudioSource(Context context){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean onlyCommons = preferences.getBoolean("only_commons",false);
+        boolean onlyTTS = preferences.getBoolean("only_tts",false);
+        boolean both = preferences.getBoolean("both",true);
+
+        if (onlyCommons){
+            return "ONLYCOMMONS";
+        }
+        else if (onlyTTS){
+            return "ONLYTTS";
+        }
+        else if (both){
+            return "BOTH";
+        }
+
         return null;
     }
 
-    public static void operate(String audioSource){
+    public static void operate(String audioSource, Context context){
+        String audioSourceSelection = selectAudioSource(context);
 
+        
     }
 
 }
